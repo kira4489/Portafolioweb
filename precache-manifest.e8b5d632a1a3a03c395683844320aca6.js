81c2a80ec80f2b8ce752d0d0b06f5953384e0f1d
Your Name <quiramapino47@gmail.com>
2020-05-18 17:18:01 -0500

    Updates

---
+++ precache-manifest.e8b5d632a1a3a03c395683844320aca6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc265f0df0a2d816a646e0e4c7d72ad2",
+    "url": "/Portafolioweb/index.html"
+  },
+  {
+    "revision": "a12104b574ab4660ea94",
+    "url": "/Portafolioweb/static/css/main.0d7b6666.chunk.css"
+  },
+  {
+    "revision": "8d965858cc75a675a37d",
+    "url": "/Portafolioweb/static/js/2.05c13572.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portafolioweb/static/js/2.05c13572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12104b574ab4660ea94",
+    "url": "/Portafolioweb/static/js/main.bee147b4.chunk.js"
+  },
+  {
+    "revision": "6da3c543e3031fea5460",
+    "url": "/Portafolioweb/static/js/runtime-main.3bab8850.js"
+  },
+  {
+    "revision": "1886ccbd6d409cf144f3dd6f1ec34266",
+    "url": "/Portafolioweb/static/media/marea.1886ccbd.jpg"
+  },
+  {
+    "revision": "862ea93fe26a26553e45806777c69b61",
+    "url": "/Portafolioweb/static/media/proyecto1.862ea93f.png"
+  },
+  {
+    "revision": "bc07217b58e8ab17f80c57baca20409b",
+    "url": "/Portafolioweb/static/media/proyecto2.bc07217b.png"
+  },
+  {
+    "revision": "9750fe127db9d915dfc6c761e766f6b2",
+    "url": "/Portafolioweb/static/media/proyecto3.9750fe12.png"
+  },
+  {
+    "revision": "4473871eb6d573453f38748a52f0cd09",
+    "url": "/Portafolioweb/static/media/react.4473871e.png"
+  }
+]);
